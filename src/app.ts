72e05f92ade38ab984d50118e40ddb5bad3543f8
sunan-rabbi <sunanrabbi1918@outlook.com>
2026-02-26 14:26:56 +0600

    action

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.get('/', async (req: Request, res: Response) => {
 
 
     res.status(200).json({
-      message: 'Hello from Custom Server: CI-CD',
+      message: 'Hello from Custom Server: CI-CD CI-CD',
       status: 'HEALTHY',
       timestamp: new Date().toISOString(),
       uptime: process.uptime(),
